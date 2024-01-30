635539a6c4705d8a2bc0735fbd58a3fb4d00f769
Matheus Rocha <matheusrochacamarg@gmail.com>
2024-01-30 20:22:45 -0300

    style: add bootstrap, sass compiler and box-icons lib

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
-import './assets/main.css';
-
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+
+import 'boxicons';
+import '@/scss/styles.scss';
 
 const app = createApp(App);
 
--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,11 +1,14 @@
 <template>
   Dashboard
+  <button class="btn btn-primary">
+    <box-icon color="white" name="rocket"></box-icon>
+    Submit
+  </button>
 </template>
 
 <script>
 export default {
   name: 'DashboardView',
-  components: {
-  }
+  components: {}
 }
 </script>
